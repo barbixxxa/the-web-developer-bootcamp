7be20bc6f81d59f99e6c022bd9b4b17379a6a74d
theeam <eduuardomuller@gmail.com>
2018-07-30 12:56:38 -0300

    S18L198

--- S18L193/assets/js/todos.js
+++ S18L193/assets/js/todos.js
@@ -1,11 +1,21 @@
-//Check off a todo by clicking
-$("li").click(function () {
+//Check off a task by clicking
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
-$("span").click(function (event) {
+//Click on x to delete a task
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+//Hit enter to add a task
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });

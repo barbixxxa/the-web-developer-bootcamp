09586a190ae57a97ae2a62d88385f9b7d95f6bba
theeam <eduuardomuller@gmail.com>
2018-08-14 07:38:08 -0300

    S28L271

--- S26L256/app.js
+++ S26L256/app.js
@@ -26,7 +26,7 @@ app.get("/campgrounds", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds", { campgrounds: campgrounds });
+            res.render("index", { campgrounds: campgrounds });
         }
     });
 });
@@ -34,10 +34,19 @@ app.get("/campgrounds", function (req, res) {
 app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
+    var description = req.body.description;
     var newCampground = {
         name: name,
-        image: image
+        image: image,
+        description: description
     };
+
+    // https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg
+    // https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg
+    // https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg
+    // https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg
+    
+
 
     Campground.create(
         newCampground, function (err, newCampground) {
@@ -50,11 +59,17 @@ app.post("/campgrounds", function (req, res) {
 });
 
 app.get("/campgrounds/new", function (req, res) {
-    res.render("new.ejs");
+    res.render("new");
 });
 
-app.get("campgrounds/:id", function (req, res) {
-
+app.get("/campgrounds/:id", function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", { campground: foundCampground });
+        }
+    });
 });
 
 app.listen(3000, function () {

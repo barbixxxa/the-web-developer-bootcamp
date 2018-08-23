7242c7b6498b6a571e3b2444020fbf91a457f2cb
theeam <eduuardomuller@gmail.com>
2018-08-23 17:43:04 -0300

    S29L282

--- S29L273/app.js
+++ S29L273/app.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    expressSanitizer = require("express-sanitizer"),
     methodOverride = require("method-override");
 
 // APP CONFIG
@@ -11,6 +12,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
@@ -50,12 +52,14 @@ app.get("/blogs/new", function (req, res) {
     res.render("new");
 });
 
-//CREATE ROUTE
-app.post("/blogs", function (req, res) {
-    Blog.create(req.body.blog, function (err, newBlog) {
-        if (err) {
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+    // create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
             res.render("new");
         } else {
+            //then, redirect to the index
             res.redirect("/blogs");
         }
     });
@@ -87,15 +91,16 @@ app.get("/blogs/:id/edit", function (req, res) {
     });
 });
 
-//UPDATE ROUTE
-app.put("/blogs/:id", function (req, res) {
-    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
-        if (err) {
-            res.redirect("/blogs");
-        } else {
-            res.redirect("/blogs/" + req.params.id);
-        }
-    });
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+      if(err){
+          res.redirect("/blogs");
+      }  else {
+          res.redirect("/blogs/" + req.params.id);
+      }
+   });
 });
 
 //DELETE ROUTE

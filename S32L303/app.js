b751b42a91fe8f2646b18c919c020fa3ba4224ce
theeam <eduuardomuller@gmail.com>
2018-08-24 18:55:22 -0300

    S32L304

--- S32L303/app.js
+++ S32L303/app.js
@@ -1,9 +1,25 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(require("express-session")({
+    secret: "The web developer bootcamp",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("home");

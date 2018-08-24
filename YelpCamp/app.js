db8d5c14befb3be9481001f2f364163022567ce7
theeam <eduuardomuller@gmail.com>
2018-08-24 14:25:42 -0300

    S31L294

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -5,11 +5,12 @@ var express = require("express"),
     Campground = require("./models/campground"),
     seedDB = require("./seeds");
 
-seedDB();
+
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+seedDB();
 
 app.set("view engine", "ejs");
 
@@ -62,8 +63,9 @@ app.get("/campgrounds/new", function (req, res) {
     res.render("new");
 });
 
+//SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function (req, res) {
-    Campground.findById(req.params.id, function (err, foundCampground) {
+    Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
         if (err) {
             console.log(err);
         } else {

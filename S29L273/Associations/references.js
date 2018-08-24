e4fef17fb0c55db64c5111cbe2715123522a94cb
theeam <eduuardomuller@gmail.com>
2018-08-24 13:03:36 -0300

    S30L289

--- S29L273/Associations/references.js
+++ S29L273/Associations/references.js
@@ -1,28 +1,11 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/restful_blog_app");
 
-//POST - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
+var Post = require("./models/post");
+var User = require("./models/user");
 
-var postModel = mongoose.model("Post", postSchema);
-
-//USER - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
-    }]
-});
-
-var User = mongoose.model("User", userSchema);
-
-postModel.create({
-    title: "how to cook",
+Post.create({
+    title: "how to cook2",
     content: "bla"
 }, function (err, post) {
     User.findOne({

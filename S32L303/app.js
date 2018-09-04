8aa2164eda984679146c80746e8b69f9f0d2e05d
theeam <eduuardomuller@gmail.com>
2018-09-04 18:06:04 -0300

    S32L305

--- S32L303/app.js
+++ S32L303/app.js
@@ -10,6 +10,9 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(require("express-session")({
     secret: "The web developer bootcamp",
     resave: false,
@@ -21,12 +24,37 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//===========
+//ROUTES
+//===========
+
+
 app.get("/", function (req, res) {
     res.render("home");
 });
 
 app.get("/secret", function (req, res) {
     res.render("secret");
+});
+
+//Auth Routes
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    User.register(new User({
+        username: req.body.username
+    }), req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/secret");
+        });
+    });
 });
 
 app.listen(3000, function () {

5cfa7e5073d5414c3a56a543f9426f48a2e794eb
theeam <eduuardomuller@gmail.com>
2018-09-07 11:53:49 -0300

    S32L306

--- S32L303/app.js
+++ S32L303/app.js
@@ -21,6 +21,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -55,6 +56,21 @@ app.post("/register", function (req, res) {
             res.redirect("/secret");
         });
     });
+});
+
+//LOGIN ROUTES
+//render login form
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+//login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function (req, res) {
+
 });
 
 app.listen(3000, function () {
